44e091ba200824616e57b9421e97f773207f94d0
shabliyartem <shabliy.artem@rezonans.ru>
2016-06-20 03:00:53 +0300

    Map and calendar

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,7 @@ module Wedding
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    Slim::Engine.set_options pretty: false
   end
 end
